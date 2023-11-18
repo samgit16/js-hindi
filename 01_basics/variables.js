9f19e721b23dfe7aa8e78f44254a6c0416ca6d62
samgit16 <145757102+samgit16@users.noreply.github.com>
2023-11-18 16:37:13 +0000

    new

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 4000;
+ accountName = "Tibo"
+ accountPassword = "2090"
+ accountCity="haldwani"
+
+ let accountStatus;
+
+/* This is comment 
+*/
+console.table([accountId,accountName,accountStatus,accountPassword,accountCity])
