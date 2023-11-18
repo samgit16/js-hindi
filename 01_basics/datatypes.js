a1ccf4f14fb72a48e94319e1fdeec5a03e16cde3
samgit16 <145757102+samgit16@users.noreply.github.com>
2023-11-18 17:30:45 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+//number => 2 to power 53 tk jata hai
+//bigint
+//string => ""
+//boolean => true/false
+undefined
+//object
+//null
+
+let Some;
+
+console.log(typeof Some)
+
+
+let none = null
+
+console.log(typeof none)
+
+//null is an object 
+
