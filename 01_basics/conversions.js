702519fe0297d3b626e72051d3cea76f17a44510
samgit16 <145757102+samgit16@users.noreply.github.com>
2023-11-21 03:21:02 +0000

    convrsion from one DT to other DT

---
+++ 01_basics/conversions.js
@@ -0,0 +1,21 @@
+let score = "30"
+//console.log(typeof score)
+
+let valueinNumber = Number(score);
+//console.log(typeof valueinNumber);
+
+
+
+//"33"=> 33
+//"33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 0
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true 0 => false
+// "" => false
+// "samarthya" => true
